414a628f5ffdb0b6cebd17eea0c5be345f08fabe
BFaycal <faycal1003@gmail.com>
2020-04-16 09:40:54 +0200

    "mettre a jour la Géolocation"

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,7 +6,7 @@ import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
 import { Camera } from '@ionic-native/camera/ngx';
-import {Geolocation} from '@ionic-native/geolocation/ngx';
+// import {Geolocation} from '@ionic-native/geolocation/ngx';
 
 @NgModule({
   imports: [
@@ -21,6 +21,6 @@ import {Geolocation} from '@ionic-native/geolocation/ngx';
     ])
   ],
   declarations: [HomePage],
-  providers: [Camera, Geolocation]
+  providers: [Camera]
 })
 export class HomePageModule {}
